f493ebcd915bde368b0dacc7d823b0546a7a136b
Polarist05 <88574175+Polarist05@users.noreply.github.com>
2024-02-25 08:26:28 +0700

    Preformance Test Method

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import timeit
 import matplotlib.pyplot as plt
 import numpy as np
 
+#---------Test Case--------------------
+
 def worstTestCase(n, iterations=1):
     times = []
     cnt = 1
@@ -16,6 +18,7 @@ def worstTestCase(n, iterations=1):
         cnt+=1
         times.append(time_taken)
     return times
+
 def bestTestCase(n, iterations=1):
     times = []
     for i in n:
@@ -24,12 +27,9 @@ def bestTestCase(n, iterations=1):
         times.append(time_taken)
     return times
 
-n_values = np.linspace(2, 202, 50)
-# Benchmark the function
-execution_times = np.multiply(worstTestCase( n_values,5),1000)
+#--------------------------------------
 
-# Define the functions to fit
-
+#------------Big O Func----------------
 def Logn(x, a, b):
     return a * np.log2(x) + b
 
@@ -47,21 +47,28 @@ def N2logN(x,a,b):
 
 def Cube(x,a,b):
     return a*x*x*x + b
+#--------------------------------------
 
-funcs = [Logn,Linear,NlogN,Square,N2logN,Cube]
+def PerformanceTest(maxValue,sampleAmount,testCase):
+    nodeCounts = np.linspace(2, maxValue, 50)
+    funcs = [Logn,Linear,NlogN,Square,N2logN,Cube]
+    execution_times = np.multiply(testCase( nodeCounts,sampleAmount),1000)
 
-# Fit curves to the data using curve_fit
-predicteds = [func(n_values,*curve_fit(func,n_values,execution_times)[0])  for func in funcs]
+    # Fit curves to the data using curve_fit
+    predictTime = [func(nodeCounts,*curve_fit(func,nodeCounts,execution_times)[0])  for func in funcs]
 
-# Calculate RMSE values
-rmses = [np.sqrt(np.mean(np.power((execution_times - predicted) , 2))) for predicted in predicteds]
+    # Calculate RMSE values
+    rmses = [np.sqrt(np.mean(np.power((execution_times - predicted) , 2))) for predicted in predictTime]
 
-# Plot the data and fitted curves
-plt.scatter(n_values, execution_times, label='Actual Data')
-for i in range(len(funcs)):
-    plt.plot(n_values, predicteds[i],label=f'({str(funcs[i].__qualname__)} RMSE={rmses[i]:.2f})')
-plt.xlabel('Node Count')
-plt.ylabel('Runtime')
-plt.title('Fitted Curves for Benchmark Data')
-plt.legend()
-plt.show()
+    # Plot the data and fitted curves
+    plt.scatter(nodeCounts, execution_times, label='Actual Data')
+    for i in range(len(funcs)):
+        plt.plot(nodeCounts, predictTime[i],label=f'({str(funcs[i].__qualname__)} RMSE={rmses[i]:.2f})')
+    plt.xlabel('Node Count')
+    plt.ylabel('Runtime')
+    plt.title('Fitted Curves for Benchmark Data')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    PerformanceTest(202,5,bestTestCase)
